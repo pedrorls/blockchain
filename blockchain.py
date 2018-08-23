8b11f3590c7d3d6f84ec52b0ed0e918275abbd6f
Pedro Silva <pedro.rls93@gmail.com>
2018-08-23 00:15:34 -0300

    Creates new Block and adds it to the chain

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,35 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        #creates the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
-        """Creates new Block and adds it to the chain"""
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Creates new Block and adds it to the chain
+        
+        :param proof: <int> The proof given by the proof of work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
