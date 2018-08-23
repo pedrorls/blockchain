3d7fa9610ae4200db5284299e7f447e6dbb3e3ed
Pedro Silva <pedro.rls93@gmail.com>
2018-08-23 00:47:08 -0300

    api blueprint with flask

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from uuid import uuid4
+from blockchain import Blockchain
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return 'We\'ll mine a new Block'
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return 'We\'ll add a new transaction'
+
+@app.route('chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonfy(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
